17f8028d9ec0911bf65e82a1c50d25745b649473
geovannabrocco-gb <geovannabrocco@gmail.com>
2026-08-26 21:47:04 -0300

    criei o o botao x e visualizar as datas de nascimento

--- aula11/user.js
+++ aula11/user.js
@@ -5,13 +5,49 @@ var usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado")) || {};
 var hi = document.getElementById("hi");
 if (hi && usuarioLogado) hi.innerHTML = "Olá " + usuarioLogado?.nome
 
-var list = document.getElementById("list") 
+var list = document.getElementById("listUser") 
 if (list){
+    let i = 0;
     users.forEach((u)=> {
-        console.log(u)
+        let tdName= document.createElement("td");
+        tdName.innerHTML= u.nome;
+        let tdEmail= document.createElement("td");
+        tdEmail.innerHTML= u.email;
+        let tdAction= document.createElement("td");
+        let btV = document.createElement("a");
+        btV.innerHTML = "V";
+        btV.classList.add("Show");
+        btV.id = i;
+        tdAction.appendChild(btV);
+
+        let span = document.createElement("span");
+        span.innerHTML = " - ";
+        tdAction.appendChild(span);
+
+        let btR = document.createElement("a");
+        btR.innerHTML = "X";
+        btR.classList.add("Remove");
+        tdAction.appendChild(btR);
+
+        let tr = document.createElement("tr");
+        tr.appendChild(tdName);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdAction);
+
+        list.appendChild(tr);
+        //i = i +;         //i += 1;
+        i++;
+
     });
 }
 
+var botoesV = document.querySelectorAll(".show");
+botoesV.forEach((b) => {
+    b.addEventListener("click", () => {
+        const id = b.id;
+        b.innerHTML = users[id].nascimento;
+    })
+})
 
 var formR = document.getElementById("formRegister");
 // get = pegar, elemento = elemento do html
